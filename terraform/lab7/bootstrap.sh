1851c5cc74f5f33064ea6b18f118c5de28fe4edd
ming <wang_ming@iclould.com>
2019-12-15 21:17:47 +0800

    add colton services

--- terraform/lab7/bootstrap.sh
+++ terraform/lab7/bootstrap.sh
@@ -20,20 +20,20 @@ isInstall(){
 
 
 sudo yum install git-all.noarch -y
-sudo yum install docker.x86_64 -y
+sudo yum install docker -y
 sudo yum install maven.noarch -y
-sudo systemctl enable docker
-if [ `echo $?` != 0 ]; then
- sudo yum install docker.x86_64 -y
- sudo systemctl enable docker
-fi
-if [ `echo $?` != 0 ]; then
-   sudo groupadd docker
-fi
-sudo gpasswd -a ${USER} docker
-sudo systemctl restart docker
+#sudo systemctl enable docker
+#if [ `echo $?` != 0 ]; then
+# sudo yum install docker.x86_64 -y
+# sudo systemctl enable docker
+#fi
+#if [ `echo $?` != 0 ]; then
+#   sudo groupadd docker
+#fi
+#sudo gpasswd -a ${USER} docker
+#sudo systemctl restart docker
 
-git clone https://github.com/chengxinjing/coltonsvc.git
+git clone https://github.com/xwolf-wang/coltonsvc.git
 #build
 cd ./coltonsvc
 mvn clean package -Dmaven.test.skip=true
