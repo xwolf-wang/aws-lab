5459a21428ffe384ede8319894337e531672f67a
ming <wang_ming@iclould.com>
2019-12-15 22:41:20 +0800

    add colton services

--- terraform/lab5/network.tf
+++ terraform/lab5/network.tf
@@ -26,6 +26,13 @@ resource "aws_security_group" "web_server_sec_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 0
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     protocol = "-1"
